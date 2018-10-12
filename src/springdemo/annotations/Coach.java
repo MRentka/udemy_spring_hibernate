655cebaf1a97728265027a170c3c794062f07baf
Michael Rentka <rentka@begasoft.ch>
2018-10-12 14:30:43 +0200

    cleanup unused files, working solution code in annotations package

--- src/springdemo/annotations/Coach.java
+++ src/springdemo/annotations/Coach.java
@@ -3,5 +3,6 @@ package springdemo.annotations;
 public interface Coach {
 
 	public String getDailyWorkout();
+	public String getDailyFortune();
 	
 }
---
+++ src/springdemo/annotations/FortuneService.java
@@ -0,0 +1,7 @@
+package springdemo.annotations;
+
+public interface FortuneService {
+
+	public String getFortune();
+	
+}
