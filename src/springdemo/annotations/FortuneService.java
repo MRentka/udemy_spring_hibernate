cec06c6168e73103296488a07123f41423bc0ab0
Michael Rentka <rentka@begasoft.ch>
2018-10-12 10:07:59 +0200

    cleaning old annotation stuff

--- src/springdemo/annotations/FortuneService.java
+++
@@ -1,8 +0,0 @@
-package springdemo.annotations;
-
-public interface FortuneService {
-	
-	public String[] fortunes = {":-) Fortune!", ":-| Average day.", ":-( Out of Luck!"};
-
-	public String getFortune();	
-}
